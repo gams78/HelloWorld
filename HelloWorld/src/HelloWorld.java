eb52ba09c96f5e33790b42e756378cb27921cd37
gams78 <boris@kalvarija.net>
2021-03-10 19:36:36 +0100

    Dodan komentar

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 
+		// Ustvarimo objekte
 		Kozarec k = new Kozarec(200);
 		Krigel kr = new Krigel(500, "Steklo", "Okrogel", false);
 		
